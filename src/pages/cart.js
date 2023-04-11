6516b2a1fbf406a39055011ff8538ce0ba8fd8a7
dominicmena <mena.dominic@gmail.com>
2023-04-11 02:26:09 -0500

    added a case for cart remove item and passed the function to the circle x button, the item is removed upon click

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -4,13 +4,18 @@ import { useContext } from "react";
 import { Layout } from "../../components/Layout";
 import Link from "next/link";
 import Image from "next/image";
-import {  } from '@heroicons/react/24/solid'
+import {} from "@heroicons/react/24/solid";
 
 export default function CartScreen() {
   const { state, dispatch } = useContext(Store);
   const {
     cart: { cartItems },
   } = state;
+
+  const removeItemHandler = (item) => {
+    dispatch({type: 'CART_REMOVE_ITEM', payload:item })
+  }
+
   return (
     <div>
       <Layout title="Shopping Cart">
@@ -49,16 +54,24 @@ export default function CartScreen() {
                           {item.name}
                         </Link>
                       </td>
-                      <td className="p-5 text-right">
-                        {item.quantity}
-                      </td>
+                      <td className="p-5 text-right">{item.quantity}</td>
                       <td className="p-5 text-right">${item.price}</td>
                       <td className="p-5 text-center">
-                        <button>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-</svg>
-
+                        <button onClick={() => removeItemHandler(item)}>
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            strokeWidth={1.5}
+                            stroke="currentColor"
+                            className="w-6 h-6"
+                          >
+                            <path
+                              strokeLinecap="round"
+                              strokeLinejoin="round"
+                              d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                            />
+                          </svg>
                         </button>
                       </td>
                     </tr>
