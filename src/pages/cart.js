0a09719805ac8c0531af422d5f000741e754b725
dominicmena <mena.dominic@gmail.com>
2023-04-11 02:19:16 -0500

    rendered circle delete button on shopping cart page by adding svg code for icon

--- src/pages/cart.js
+++ src/pages/cart.js
@@ -3,8 +3,8 @@ import { Store } from "../../utils/Store";
 import { useContext } from "react";
 import { Layout } from "../../components/Layout";
 import Link from "next/link";
-import { XCircleIcon } from '@heroicons/react/outline/XCircleIcon'
 import Image from "next/image";
+import {  } from '@heroicons/react/24/solid'
 
 export default function CartScreen() {
   const { state, dispatch } = useContext(Store);
@@ -55,7 +55,10 @@ export default function CartScreen() {
                       <td className="p-5 text-right">${item.price}</td>
                       <td className="p-5 text-center">
                         <button>
-                            
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+</svg>
+
                         </button>
                       </td>
                     </tr>
