5457062985bf88226dbd73c0db5b239b012bb05a
dominicmena <mena.dominic@gmail.com>
2023-04-11 00:26:30 -0500

    created vinyl image with a userouter hook, pulled the slug and then accessed the image for the sour ce and set a grid with columns and sizing for md screens

--- src/pages/vinyl/[slug].js
+++ src/pages/vinyl/[slug].js
@@ -3,6 +3,8 @@ import React from 'react'
 import { Layout } from '../../../components/Layout'
 import { useEffect } from 'react'
 import data from '../../../utils/data'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default function VinylScreen() {
     const {query} = useRouter()
@@ -16,7 +18,21 @@ export default function VinylScreen() {
     }, [])
   return (
     <Layout title={vinyl.name}>
-        <h1>{vinyl.name}</h1>
+        <div className="py-2">
+            <Link href='/'>back to all vinyl</Link>
+        </div>
+        <div className='grid md:grid-cols-4 md:gap-3'>
+            <div className='md:col-span-2'>
+                <Image
+                src={vinyl.image}
+                alt={vinyl.name}
+                width={640}
+                height={640}
+                Layout='responsive'>
+                    
+                </Image>
+            </div>
+        </div>
     </Layout>
   )
 }
