964757b0e5b4fc2ebbd04ba9c51fda0ef47ef71d
dominicmena <mena.dominic@gmail.com>
2023-04-11 14:24:29 -0500

    rendered conditional error messages for email and password

--- src/pages/login.js
+++ src/pages/login.js
@@ -23,7 +23,12 @@ export default function LoginScreen() {
           <label htmlFor="email">Email</label>
           <input
             type="email"
-            {...register("email", { required: "Please enter email" })}
+            {...register("email", { required: "Please enter email", 
+        pattern: {
+            value: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$/i,
+            message: 'Please enter valid email address',
+        }
+        })}
             className="w-full"
             id="email"
             autoFocus
@@ -36,10 +41,17 @@ export default function LoginScreen() {
           <label htmlFor="password">Password</label>
           <input
             type="password"
+            {...register('password', {
+                required: 'Please enter password',
+                minLength: { value: 6, message: 'password is more than 5 chars'},
+            })}
             className="w-full"
             id="password"
             autoFocus
           ></input>
+          {errors.password && (
+            <div className="text-red-500">{errors.password.message}</div>
+          )}
         </div>
         <div className="mb-4">
           <button className="primary-button">Login</button>
