ff535bf04d8a633b4492ee00304ceabba126dff6
dominicmena <mena.dominic@gmail.com>
2023-04-11 14:10:16 -0500

    added a submit handler function to display an error message if the email input is not filled in

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,29 +1,54 @@
-import Link from 'next/link'
-import React from 'react'
-import { Layout } from '../../components/Layout'
+import Link from "next/link";
+import React from "react";
+import { Layout } from "../../components/Layout";
+import { useForm } from "react-hook-form";
 
 export default function LoginScreen() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm();
+
+  const submitHandler = ({ email, password }) => {};
+
   return (
-    <Layout title='Login'>
-        <form className='mx-auto max-w-screen-md'>
-            <h1 className='mb-4 text-xl'>Login</h1>
-            <div className='mb-4'>
-                <label htmlFor='email'>Email</label>
-                <input type='email' className='w-full' id='email' autoFocus></input>
-            </div>
-            <div className='mb-4'>
-                <label htmlFor='password'>Password</label>
-                <input type='password' className='w-full' id='password' autoFocus></input>
-            </div>
-            <div className='mb-4'>
-                
-                <button className='primary-button'>Login</button>
-            </div>
-            <div className='mb-4'>
-                Don&apos;t have an account? &nbsp;
-                <Link href='register'>Register</Link>
-            </div>
-        </form>
+    <Layout title="Login">
+      <form
+        className="mx-auto max-w-screen-md"
+        onSubmit={handleSubmit(submitHandler)}
+      >
+        <h1 className="mb-4 text-xl">Login</h1>
+        <div className="mb-4">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            {...register("email", { required: "Please enter email" })}
+            className="w-full"
+            id="email"
+            autoFocus
+          ></input>
+          {errors.email && (
+            <div className="text-red-500">{errors.email.message}</div>
+          )}
+        </div>
+        <div className="mb-4">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            className="w-full"
+            id="password"
+            autoFocus
+          ></input>
+        </div>
+        <div className="mb-4">
+          <button className="primary-button">Login</button>
+        </div>
+        <div className="mb-4">
+          Don&apos;t have an account? &nbsp;
+          <Link href="register">Register</Link>
+        </div>
+      </form>
     </Layout>
-  )
+  );
 }
