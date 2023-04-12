3c8ebd8ce563a5fdadaf97392608b52dfbfa6aff
dominicmena <mena.dominic@gmail.com>
2023-04-12 18:25:05 -0500

    got a dropdown link to generate so the profile shows after login

--- components/Layout.js
+++ components/Layout.js
@@ -7,8 +7,10 @@ import data from "../utils/data";
 import { useState } from "react";
 import { useEffect } from "react";
 import { ToastContainer } from "react-toastify";
+import { Menu } from "@headlessui/react";
 import { useSession } from "next-auth/react";
 import 'react-toastify/dist/ReactToastify.css'
+import DropdownLink from "./DropdownLink";
 
 export function Layout({ title, children }) {
   const { status, data: session } = useSession();
@@ -47,7 +49,18 @@ export function Layout({ title, children }) {
               {status === "loading" ? (
                 "Loading"
               ) : session?.user ? (
-                session.user.name
+                <Menu as='div' className={'relative inline-block'}>
+                    <Menu.Button className='text-blue-600'>
+                        {session.user.name}
+                    </Menu.Button>
+                    <Menu.Items className='absolute right-0 w-56 origin-top-right bg-white shadow-lg'>
+                        <Menu.Item>
+                            <DropdownLink className='dropdown-link' href='/profile'>
+                                Profile
+                            </DropdownLink>
+                        </Menu.Item>
+                    </Menu.Items>
+                </Menu>
               ) : (
                 <Link href="/login" className="p-2">
                   Login
