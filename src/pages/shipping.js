bab0ff711e7775e49ca32b70be31455dd1f9dd89
dominicmena <mena.dominic@gmail.com>
2023-04-12 21:37:04 -0500

    added a form and rendering first input for full name with the useForm hook

--- src/pages/shipping.js
+++ src/pages/shipping.js
@@ -1,13 +1,47 @@
 import React from 'react'
 import CheckoutWizard from '../../components/CheckoutWizard'
 import { Layout } from '../../components/Layout'
+import { useForm } from 'react-hook-form'
 
 
 
 export default function ShippingScreen() {
+    const {
+        handleSubmit,
+        register,
+        formState: { errors },
+        setValue,
+        getValues,
+    } = useForm()
+
+    const submitHandler = () => {
+
+    }
+
   return (
     <Layout title ='Shipping Address'>
         <CheckoutWizard activeStep={1} />
+        <form 
+        className='mx-auto max-w-screen-md'
+        onSubmit={handleSubmit(submitHandler)}>
+            <h1 className='mb-4 text-xl'>Shipping Address</h1>
+            <div className='mb-4'>
+            <label htmlFor='fullName'>Full Name</label>
+            <input
+            className='w-full'
+            id='fullName'
+            autoFocus
+            {...register('fullName', {
+                required: 'Please enter full name',
+            })
+
+            }
+            />
+            {errors.fullName && (
+                <div className='text-red-500'>{errors.fullName.message}</div>
+            )}
+            </div>
+        </form>
     </Layout>
   )
 }
