95c6678b767b9e74429f0d459af5f03961d8d943
dominicmena <mena.dominic@gmail.com>
2023-04-13 14:07:12 -0500

    updated placeorder screen with a table to show order items and an edit button to redirect to the cart screen

--- src/pages/placeorder.js
+++ src/pages/placeorder.js
@@ -3,6 +3,7 @@ import React, { useContext } from 'react'
 import CheckoutWizard from '../../components/CheckoutWizard'
 import { Layout } from '../../components/Layout'
 import { Store } from '../../utils/Store'
+import Image from 'next/image'
 
 export default function PlaceOrderScreen() {
     const { state, dispatch } = useContext(Store)
@@ -30,6 +31,52 @@ export default function PlaceOrderScreen() {
                         </div>
                         <div>
                             <Link href='/shipping'>Edit</Link>
+                        </div>
+                    </div>
+                    <div className='card p-5'>
+                        <h2 className='mb-2 text-lg'>Payment Method</h2>
+                        <div>{paymentMethod}</div>
+                        <div>
+                            <Link href='/payment'>Edit </Link>
+                        </div>
+                    </div>
+                    <div className='card overflow-x-auto p-5'>
+                        <h2 className='mb-2 text-lg'>Order Items</h2>
+                        <table className='min-w-full'>
+                            <thead className='border-b'>
+                            <tr>
+                                <th className='px-5 text-left'>Item</th>
+                                <th className='p-5 text-right'>Quantity</th>
+                                <th className='p-5 text-right'>Price</th>
+                                <th className='p-5 text-right'>Subtotal</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                                {cartItems.map((item) => (
+                                    <tr key={item._id} className='border-b'>
+                                        <td>
+                                            <Link href={`/vinyl/${item.slug}`} className='flex items-center'>
+                                                <Image
+                                                src={item.image}
+                                                alt={item.name}
+                                                width={50}
+                                                height={50}>
+
+                                                </Image>
+                                                &nbsp;
+                                                {item.name}
+
+                                            </Link>
+                                        </td>
+                                        <td className='p-5 text-right'>{item.quantity}</td>
+                                        <td className='p-5 text-right'>${item.price}</td>
+                                        <td className='p-5 text-right'>${item.quantity * item.price}</td>
+                                    </tr>
+                                ))}
+                            </tbody>
+                        </table>
+                        <div>
+                            <Link href='/cart'>Edit </Link>
                         </div>
                     </div>
                 </div>
