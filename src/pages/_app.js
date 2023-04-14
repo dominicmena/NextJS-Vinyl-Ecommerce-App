f7291544201d18817ae72f031f575b5da3d4b602
dominicmena <mena.dominic@gmail.com>
2023-04-14 11:56:35 -0500

    adding paypal features, included the env client i d, imported buttons, added cases for payment, and functions for createorder, onapprove and oner ror

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -2,11 +2,13 @@ import '@/styles/globals.css'
 import { StoreProvider } from '../../utils/Store'
 import { SessionProvider, useSession } from 'next-auth/react'
 import { useRouter } from 'next/router'
+import { PayPalScriptProvider } from '@paypal/react-paypal-js'
 
 export default function App({ Component, pageProps: { session, ...pageProps} }) {
   return (
     <SessionProvider session={session}>
   <StoreProvider>
+    <PayPalScriptProvider deferLoading={true}>
     {Component.auth? (
       <Auth>
         <Component {...pageProps}/>
@@ -14,7 +16,8 @@ export default function App({ Component, pageProps: { session, ...pageProps} }) 
     )
     : (
     <Component {...pageProps}/>)}
-    
+    </PayPalScriptProvider>
+
   </StoreProvider>
   </SessionProvider>
   )
