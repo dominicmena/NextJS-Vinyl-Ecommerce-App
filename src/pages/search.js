ffda6bc42840b919d388daeaeded3d7adba061dd
dominicmena <mena.dominic@gmail.com>
2023-04-29 12:23:02 -0500

    added all ternaries for filters and props

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,4 +1,10 @@
+import { XCircleIcon } from "@heroicons/react/24/solid";
+import axios from "axios";
 import { useRouter } from "next/router";
+import { toast } from "react-toastify";
+import { Layout } from "../../components/Layout";
+import ProductItem from "../../components/ProductItem";
+import { Store } from "../../utils/Store";
 
 const PAGE_SIZE = 2;
 
@@ -61,21 +67,210 @@ export default function Search(props) {
     });
   };
   const categoryHandler = (e) => {
-    filterSearch({category: e.target.value })
-  }
+    filterSearch({ category: e.target.value });
+  };
   const pageHandler = (page) => {
-    filterSearch({ page })
-  }
+    filterSearch({ page });
+  };
   const brandHandler = (e) => {
-    filterSearch({brand: e.target.value })
-  }
+    filterSearch({ brand: e.target.value });
+  };
   const sortHandler = (e) => {
-    filterSearch({sort: e.target.value })
-  }
+    filterSearch({ sort: e.target.value });
+  };
   const priceHandler = (e) => {
-    filterSearch({price: e.target.value })
-  }
+    filterSearch({ price: e.target.value });
+  };
   const ratingHandler = (e) => {
-    filterSearch({rating: e.target.value })
-  }
+    filterSearch({ rating: e.target.value });
+  };
+
+  const { state, dispatch } = useContext(Store);
+  const addToCartHandler = async (product) => {
+    const existItem = state.cart.cartItems.find((x) => x._id === product._id);
+    const quantity = existItem ? existItem.quantity + 1 : 1;
+    const { data } = await axios.get(`/api/products/${product._id}`);
+    if (data.countInStock < quantity) {
+      toast.error("Sorry. Item is out of stock");
+      return;
+    }
+    dispatch({ type: " CART_ADD_ITEM", payload: { ...product, quantity } });
+    router.push("/cart");
+  };
+
+  return (
+    <Layout title="search">
+      <div className="grid md:grid-cols-4 md:gap-5">
+        <div>
+          <div className="my-3">
+            <h2>Categories</h2>
+            <select
+              className="w-full"
+              value={category}
+              onChange={categoryHandler}
+            >
+              <option value="all"></option>
+              {categories &&
+                categories.map((category) => (
+                  <option key={category} value={category}>
+                    {category}
+                  </option>
+                ))}
+            </select>
+          </div>
+          <div className="mb-3">
+            <h2>Brands</h2>
+            <select className="w-full" value={brand} onChange={brandHandler}>
+              <option value="all">All</option>
+              {brands &&
+                brands.map((brand) => (
+                  <option key={brand} value={brand}>
+                    {brand}
+                  </option>
+                ))}
+            </select>
+          </div>
+          <div className="mb-3">
+            <h2>Prices</h2>
+            <select className="w-full" value={price} onChange={priceHandler}>
+              <option value="all">All</option>
+              {prices &&
+                prices.map((price) => (
+                  <option key={price.value} value={price.value}>
+                    {price.name}
+                  </option>
+                ))}
+            </select>
+          </div>
+          <div className="mb-3">
+            <h2>Ratings</h2>
+            <select className="w-full" value={rating} onChange={ratingHandler}>
+              <option value="all">All</option>
+              {ratings &&
+                ratings.map((rating) => (
+                  <option key={rating} value={rating}>
+                    {rating} star{rating > 1 && "s"} & up
+                  </option>
+                ))}
+            </select>
+          </div>
+        </div>
+        <div className="md:col-span-3">
+          <div className="mb-2 flex items-center justify-between border-b-2 pb-2">
+            <div className="flex items-center">
+              {products.length === 0 ? "No" : countProducts} Results
+              {query !== "all" && query !== "" && " : " + query}
+              {category !== "all" && " : " + category}
+              {brand !== "all" && " : " + brand}
+              {price !== "all" && " : Price " + price}
+              {rating !== "all" && " :  Rating" + rating + " & up"}
+              &nbsp;
+              {(query !== "all" && query !== "") ||
+              category !== "all" ||
+              brand !== "all" ||
+              rating !== "all" ||
+              price !== "all" ? (
+                <button onClick={() => router.push("/search")}>
+                  <XCircleIcon className="h-5 w-5" />
+                </button>
+              ) : null}
+            </div>
+            <div>
+              Sort by{" "}
+              <select value={sort} onChange={sortHandler}>
+                <option value="featured">Featured</option>
+                <option value="lowest">Price: Low to High</option>
+                <option value="highest">Price: High to Low</option>
+                <option value="toprated">Customer Reviews</option>
+                <option value="newest">Newest Arrivals</option>
+              </select>
+            </div>
+          </div>
+          <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+                {products.map((product) => (
+                    <ProductItem
+                    key={product._id}
+                    product={product}
+                    addToCartHandler={addToCartHandler}
+                    />
+                ))}
+          </div>
+          <ul className="flex">
+            {products.length > 0 &&
+            [...Array(pages).keys()].map((pageNumber) => (<li key={pageNumber}>
+                <button
+                className={`default-button m-2 ${
+                    page == pageNumber + 1 ? 'font-bold' : ''
+                }`}
+                onClick={() => pageHandler(pageNumber + 1)}>
+                    {pageNumber + 1}
+                </button>
+            </li>))}
+          </ul>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+export async function getServerSideProps({query}) {
+    const pageSize = query.pageSize || PAGE_SIZE;
+    const page = query.page || 1;
+    const category = query.category || '';
+    const brand = query.brand || '';
+    const price = query.price || '';
+    const rating = query.rating || '';
+    const sort = query.sort || '';
+    const searchQuery = query.query || '';
+
+    const queryFilter = 
+    searchQuery && searchQuery !== 'all'
+    ? {
+        name: {
+            $regex: searchQuery,
+            $options: 'i',
+        },
+    }
+    : {}
+    const categoryFilter = category && category !== 'all' ? { category } : {};
+    const brandFilter = brand && brand !== 'all' ? { brand } : {};
+    const ratingFilter = rating && rating !== 'all'
+    ? {
+        rating: {
+            $gte: Number(rating),
+        },
+    }
+    : {};
+    const priceFilter = price && price !== 'all'
+    ? {
+        price: {
+            $gte: Number(price.split('-')[0]),
+            $lte: Number(price.split('-')[1]),
+        },
+    }
+    : {}
+    const order = 
+    sort === 'featured'
+    ? { isFeatured: -1}
+    : sort === 'lowest' ? {price: 1}
+    : sort === 'highest' ? {price: -1}
+    : sort === 'toprated' ? {rating: -1}
+    : sort === 'newest' ? {createdAt: -1}
+    : {_id: -1}
+    const categories = await Product.find().distinct('category')
+    const brands = await Product.find().distinct('brand')
+    const productDocs = await Product.find(
+        {
+            ...queryFilter,
+            ...categoryFilter,
+            ...priceFilter,
+            ...brandFilter,
+            ...ratingFilter
+        },
+        '-reviews'
+    )
+    .sort(order)
+    .skip(pageSize * (page - 1))
+    .limit(pageSize)
+    .lean()
 }
