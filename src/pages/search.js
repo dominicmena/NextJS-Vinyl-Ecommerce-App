e05f81a6902fa821cedb5aaa36c506f2af5b637c
dominicmena <mena.dominic@gmail.com>
2023-04-29 11:29:41 -0500

    added handlers for the UI

--- src/pages/search.js
+++ src/pages/search.js
@@ -1,57 +1,81 @@
-import { useRouter } from "next/router"
+import { useRouter } from "next/router";
 
-
-const PAGE_SIZE = 2
+const PAGE_SIZE = 2;
 
 const prices = [
-    {
-        name: '$1 to $50',
-        value: '1-50',
-    },
-    {
-        name: '$51 to $200',
-        value: '51-200',
-    },
-    {
-        name: '$201 to $1000',
-        value: '201-1000',
-    },
-]
+  {
+    name: "$1 to $50",
+    value: "1-50",
+  },
+  {
+    name: "$51 to $200",
+    value: "51-200",
+  },
+  {
+    name: "$201 to $1000",
+    value: "201-1000",
+  },
+];
 
-const ratings = [1, 2, 3, 4, 5]
+const ratings = [1, 2, 3, 4, 5];
 
 export default function Search(props) {
-    const router = useRouter()
-    const {
-        query= 'all',
-        category = 'all',
-        brand = 'all',
-        price = 'all',
-        rating = 'all',
-        sort = 'all',
-        page = 1,
-    } = router.query
+  const router = useRouter();
+  const {
+    query = "all",
+    category = "all",
+    brand = "all",
+    price = "all",
+    rating = "all",
+    sort = "all",
+    page = 1,
+  } = router.query;
 
-    const { products, countProducts, categories, brands, pages } = props
+  const { products, countProducts, categories, brands, pages } = props;
 
-    const filterSearch = ({
-        page,
-        category, 
-        brand,
-        sort,
-        min,
-        max,
-        searchQuery,
-        price,
-        rating,
-    }) => {
-        const {query} = router
-        if (page) query.page = page
-        if (searchQuery) query.searchQuery = searchQuery
-        if (sort) query.sort = sort
-        if (category) query.category = category
-        if (brand) query.brand = brand
-        if (price) query.price = price
-        if (rating) query.rating = rating
-    }
+  const filterSearch = ({
+    page,
+    category,
+    brand,
+    sort,
+    min,
+    max,
+    searchQuery,
+    price,
+    rating,
+  }) => {
+    const { query } = router;
+    if (page) query.page = page;
+    if (searchQuery) query.searchQuery = searchQuery;
+    if (sort) query.sort = sort;
+    if (category) query.category = category;
+    if (brand) query.brand = brand;
+    if (price) query.price = price;
+    if (rating) query.rating = rating;
+    if (min) query.min ? query.min : query.min === 0 ? 0 : min;
+    if (max) query.max ? query.max : query.max === 0 ? 0 : max;
+
+    router.push({
+      pathname: router.pathname,
+      query: query,
+    });
+  };
+  const categoryHandler = (e) => {
+    filterSearch({category: e.target.value })
+  }
+  const pageHandler = (page) => {
+    filterSearch({ page })
+  }
+  const brandHandler = (e) => {
+    filterSearch({brand: e.target.value })
+  }
+  const sortHandler = (e) => {
+    filterSearch({sort: e.target.value })
+  }
+  const priceHandler = (e) => {
+    filterSearch({price: e.target.value })
+  }
+  const ratingHandler = (e) => {
+    filterSearch({rating: e.target.value })
+  }
 }
